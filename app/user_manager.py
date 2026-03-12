8ea2811c5b0845bbdea270268a2e489e7858531d
hiba <touikhiba@gmail.com>
2026-03-12 11:15:41 +0000

    etape2

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,6 +1,10 @@
 class UserManager:
+    
+    
     def __init__(self):
        self.users = []
+       
+       
     
     def add_user(self, username):
         if not username:
@@ -9,14 +13,21 @@ class UserManager:
             raise ValueError ("utilisateur déja existants")
         self.users.append(username)
         
+        
+        
     def remove_user(self, username):
         if username not in self.users:
             raise ValueError("Utilisateur introvable")
         self.users.remove(username)
     
+    
+    
     def count_users(self):
         return len(self.users)
+    
+    
     
 def count_total_users(users):
     temp =0
     return len(users)
+
